f6d20a96b8c6ba5a011b2d93a01c9364f762a378
macmenak <macmenak@gmail.com>
2018-06-29 00:26:58 +0200

    Add results strict ingredients filter and some refacturing

---
+++ src/js/RecipesIngredientsList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import {
+  Segment,
+  Button,
+  Header,
+  Icon,
+  List,
+  Divider,
+  Checkbox
+} from 'semantic-ui-react';
+
+import {
+    Link
+  } from 'react-router-dom';
+
+class RecipesIngredientsList extends Component {
+
+    render() {
+        const {ingredients} = this.props;
+        return (
+        <Segment >
+            <Link to="/" style={{color: "#ffffff"}}>
+                <Button fluid size='large' icon='left arrow' labelPosition='left' content='Change ingredients' /> 
+            </Link>
+            <Divider />
+            <Header as='h2'>Selected ingredients</Header>
+            <List relaxed verticalAlign='middle' size='big'>
+                {Array.from(ingredients).map(ingredient => (
+                <List.Item key={ingredient}>
+                    <Icon name='check circle' color='green' />
+                    <List.Content>
+                        {ingredient}
+                    </List.Content>
+                </List.Item>
+                ))}
+            </List>
+            <Divider />
+            <Checkbox onChange={this.props.toggleStrictResults} toggle defaultChecked label='recipes with ingredients not on the list'/>
+            
+          
+        </Segment>
+      );
+    }
+  }
+
+export default RecipesIngredientsList;
---
+++ src/js/RecipesResults.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import {
+  Segment,
+  Button,
+  Header,
+  Icon,
+  Item,
+  Label,
+  Loader,
+  Container
+} from 'semantic-ui-react';
+
+
+const NoResults = () => (
+    <Container text textAlign='center'>
+        <Icon name='search' size='huge' color='grey'/>
+        <Header as='h2'>Sorry... we haven't found any recipes.</Header>
+    </Container>
+)
+
+class RecipesResults extends Component {
+    
+
+    render() {
+        const {recipes, loading, ingredients, strictResults} = this.props;
+        const placeholder = loading ? <Loader active={loading} inline='centered'>Loading </Loader> : <NoResults/>;
+        const selectedIngredients = Array.from(ingredients);
+        function getArrayUniqueValues(array) {
+            const set = new Set(array);
+            return Array.from(set);
+        }
+        let results = [];
+        results = recipes.filter(recipe => {
+            let notStrictIngredients = false;
+            getArrayUniqueValues(recipe.ingredients.split(', ')).forEach(ingredient => {
+                if (selectedIngredients.indexOf(ingredient)<0) notStrictIngredients = true;
+            });
+            if (!(strictResults && notStrictIngredients)) return recipe;
+        });        
+        
+        return (results && results.length<=0) ? placeholder : (
+            <div>
+                <Header as='h2'>Found recipes</Header>
+                <Item.Group divided>
+                    {results.map(recipe => 
+                    {
+                    
+                    const thumbnail = recipe.thumbnail.length>0 ? <Item.Image size='small'  as='a' href={recipe.href} style={{backgroundImage: `url(${recipe.thumbnail})`, backgroundSize: 'cover', display: 'block', width: '150px', height: '150px' }} /> : <Item.Image size='small' ><Segment disabled textAlign='center' style={{height: '150px', paddingTop: '2em'}}><Icon name='image' size='huge' /> <p>photo not available</p></Segment></Item.Image>;
+                    
+                    return (
+                            <Item key={recipe.href}>
+                                {thumbnail}
+                                <Item.Content verticalAlign='middle'>
+                                    <Item.Header as='a' href={recipe.href}>{recipe.title}</Item.Header>
+                                    <Item.Meta>
+                                        <p><Icon name='tag' /> Ingredients:</p>
+                                        <Label.Group>
+                                            {getArrayUniqueValues(recipe.ingredients.split(', ')).map(ingredient => (
+                                            selectedIngredients.indexOf(ingredient)>=0 ? <Label key={ingredient}>{ingredient}</Label> : <Label key={ingredient} basic color='blue'>{ingredient}</Label>
+                                            ))}
+                                        </Label.Group>
+                                    </Item.Meta>
+                                    <Item.Extra>
+                                        <Button primary basic floated='right' href={recipe.href}>
+                                            Go to recipe's site
+                                            <Icon name='right chevron' />
+                                        </Button>
+                                    </Item.Extra>
+                                </Item.Content>
+                            </Item>
+                        )
+                    })}
+                </Item.Group>
+            </div>
+        );
+    }
+    
+}
+
+export default RecipesResults
